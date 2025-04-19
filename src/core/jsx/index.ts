c8deb1340cabfd90837152c4de6e4f87527525f4
velzie <velzie@velzie.rip>
2025-04-18 22:11:39 -0400

    class component test

--- src/core/jsx/index.ts
+++ src/core/jsx/index.ts
@@ -4,19 +4,24 @@ import { VNode } from "./vdom";
 // jsx definitions
 import "./jsx";
 
-export { render, VNode } from "./vdom";
+export { render, VNode, Component } from "./vdom";
 
-function jsxFactory(type: any, props: { [index: string]: any } | null, ...children: (VNode | string)[]): VNode {
-	dev: {
-		if (!["string", "function"].includes(typeof type)) throw "invalid component";
-	}
+function jsxFactory(
+  type: any,
+  props: { [index: string]: any } | null,
+  ...children: (VNode | string)[]
+): VNode {
+  dev: {
+    if (!["string", "function"].includes(typeof type))
+      throw "invalid component";
+  }
 
-	return {
-		[DREAMLAND]: VNODE,
-		_init: type,
-		_children: children,
-		_props: props,
-	}
+  return {
+    [DREAMLAND]: VNODE,
+    _init: type,
+    _children: children,
+    _props: props,
+  };
 }
 
 export let h = jsxFactory;
--- src/core/jsx/vdom.ts
+++ src/core/jsx/vdom.ts
@@ -2,80 +2,91 @@ import { DREAMLAND, VNODE } from "../consts";
 import { createState, DLBasePointer, isBasePtr } from "../state";
 
 export type VNode = {
-	[DREAMLAND]: typeof VNODE,
-	// @internal
-	_init: string | Function,
-	// @internal
-	_children: (VNode | string | DLBasePointer<any>)[],
-	// @internal
-	_props: Record<string, any>,
+  [DREAMLAND]: typeof VNODE;
+  // @internal
+  _init: string | (new () => Component);
+  // @internal
+  _children: (VNode | string | DLBasePointer<any>)[];
+  // @internal
+  _props: Record<string, any>;
 
-	// @internal
-	_rendered?: HTMLElement,
+  // @internal
+  _rendered?: HTMLElement;
 };
 
 function isVNode(val: any): val is VNode {
-	return val[DREAMLAND] === VNODE;
+  return val[DREAMLAND] === VNODE;
 }
 
-type ComponentChild = VNode | string | number | boolean | null | undefined | DLBasePointer<ComponentChild>;
+type ComponentChild =
+  | VNode
+  | string
+  | number
+  | boolean
+  | null
+  | undefined
+  | DLBasePointer<ComponentChild>;
 
 function mapChild(child: ComponentChild): Node {
-	if (child == null) {
-		return document.createComment("");
-	} else if (isBasePtr(child)) {
-		let childEl: Node = null!;
+  if (child == null) {
+    return document.createComment("");
+  } else if (isBasePtr(child)) {
+    let childEl: Node = null!;
 
-		function setNode(val: ComponentChild) {
-			let newEl: Node = mapChild(val);
-			childEl?.parentNode.replaceChild(newEl, childEl);
-			childEl = newEl;
-		}
+    function setNode(val: ComponentChild) {
+      let newEl: Node = mapChild(val);
+      childEl?.parentNode.replaceChild(newEl, childEl);
+      childEl = newEl;
+    }
 
-		setNode(child.value);
-		child.listen(setNode);
-		return childEl;
-	} else if (isVNode(child)) {
-		return render(child);
-	} else {
-		return document.createTextNode("" + child);
-	}
+    setNode(child.value);
+    child.listen(setNode);
+    return childEl;
+  } else if (isVNode(child)) {
+    return render(child);
+  } else {
+    return document.createTextNode("" + child);
+  }
+}
+
+export class Component {
+  css?: string;
+  html: VNode;
+  constructor() {
+    return createState(this);
+  }
 }
 
 export function render(node: VNode): HTMLElement {
-	dev: {
-		if (!isVNode(node)) {
-			throw "render requires a vnode";
-		}
-	}
+  dev: {
+    if (!isVNode(node)) {
+      throw "render requires a vnode";
+    }
+  }
 
-	if (node._rendered) return node._rendered;
+  if (node._rendered) return node._rendered;
 
-	if (typeof node._init === "function") {
-		let state = createState({
-			...node._props,
-			children: node._children,
-		});
-		let tree = node._init.call(state);
+  if (typeof node._init === "function") {
+    let ins = new node._init();
 
-		return render(tree);
-	} else {
-		let el = document.createElement(node._init);
-		node._rendered = el;
+    return render(ins.html);
+  } else {
+    let el = document.createElement(node._init);
+    node._rendered = el;
 
-		for (let attr in node._props) {
-			let val = node._props[attr];
-			if (attr.startsWith("on:")) {
-				el.addEventListener(attr.substring(3), val);
-			} else {
-				el.setAttribute(attr, val);
-			}
-		}
+    for (let attr in node._props) {
+      let val = node._props[attr];
+      if (attr.startsWith("on:")) {
+        el.addEventListener(attr.substring(3), val);
+      } else {
+        el.setAttribute(attr, val);
+      }
+    }
 
-		for (let child of node._children) {
-			el.appendChild(mapChild(child));
-		}
+    for (let child of node._children) {
+      el.appendChild(mapChild(child));
+    }
 
-		return el;
-	}
+    return el;
+  }
 }
