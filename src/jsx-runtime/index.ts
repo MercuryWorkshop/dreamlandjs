f832d58659389f72e4c7c84086fdf7fab59535bc
Toshit Chawda <r58playz@gmail.com>
2025-05-17 14:49:19 -0700

    golfing

--- src/jsx-runtime/index.ts
+++ src/jsx-runtime/index.ts
@@ -5,11 +5,8 @@ function jsx(
 	props: Record<string, any> | null,
 	key: string | undefined
 ): HTMLElement {
-	let mapped = Object.fromEntries(
-		Object.entries(props).filter((x) => x[0] !== "children")
-	);
+	let { children, ...mapped } = props;
 	if (key) mapped.key = key;
-	let children = props.children;
 	return h(
 		type,
 		mapped,
